b4a0f34b81d992fb8379e587d6bd4ba580238eac
Anton@habdevs <hramickih@yandex.ru>
2024-06-13 02:49:01 +0500

    chore: test page

--- host/src/app/page.tsx
+++ host/src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.scss';
 
 export default function Home() {
+  const one: any = 0;
   return (
     <main className={styles.main}>
       <div className={styles.hello}>HI TG apps</div>
