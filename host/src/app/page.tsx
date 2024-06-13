ce5986fec03ae2d301b4e495e3f158867b2d6b90
Anton@habdevs <hramickih@yandex.ru>
2024-06-13 12:19:23 +0500

    chore: fixed lint

--- host/src/app/page.tsx
+++ host/src/app/page.tsx
@@ -1,9 +1,6 @@
 import styles from './page.module.scss';
 
 export default function Home() {
-  const one: any = 10;
-  console.log(one, "onesss")
-
   return (
     <main className={styles.main}>
       <div className={styles.hello}>HI TG apps</div>
